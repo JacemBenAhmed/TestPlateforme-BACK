531fa91a59a23812e10c0ce730151dfeded55340
JacemBenAhmed <jacem0987654321@gmail.com>
2025-02-21 14:54:03 +0100

    fix run-job api

--- config/dotenvConfig.js
+++ config/dotenvConfig.js
@@ -5,6 +5,7 @@ module.exports = {
     PORT: process.env.PORT  ,
     JENKINS_URL:process.env.JENKINS_URL,
     JENKINS_API_TOKEN:process.env.JENKINS_API_TOKEN,
-    JENKINS_JOB_NAME:process.env.JENKINS_API_NAME,
-    JENKINS_USER:process.env.JENKINS_USER
+    JENKINS_JOB_NAME:process.env.JENKINS_JOB_NAME,
+    JENKINS_USER:process.env.JENKINS_USER ,
+    JENKINS_API_TOKEN2:process.env.JENKINS_API_TOKEN2
 };
--- controllers/jenkinsController.js
+++ controllers/jenkinsController.js
@@ -1,21 +1,20 @@
 const axios = require('axios');
-const { JENKINS_URL, JENKINS_USER, JENKINS_API_TOKEN, JENKINS_JOB_NAME } = require('../config/dotenvConfig');
+const { JENKINS_URL, JENKINS_USER, JENKINS_API_TOKEN, JENKINS_JOB_NAME ,JENKINS_API_TOKEN2} = require('../config/dotenvConfig');
 
 exports.runJenkinsJob = async (req, res) => {
     try {
-        const url = `${JENKINS_URL}/job/${JENKINS_JOB_NAME}/build?token=${JENKINS_API_TOKEN}`;
-
-
+        const url = `${JENKINS_URL}/job/${JENKINS_JOB_NAME}/build`;
+        console.log(url) ;
         const response = await axios.post(url, {}, {
             auth: {
                 username: JENKINS_USER,
-                password: JENKINS_API_TOKEN
+                password: JENKINS_API_TOKEN2
             }
         });
 
         res.json({ message: "Job Jenkins déclenché avec succès", status: response.status });
     } catch (error) {
-        res.status(500).json({ error: "Erreur lors du déclenchement du job", details: error.response?.data || error.message });
+        
     }
 };
 
@@ -30,14 +29,16 @@ exports.getJenkinsJobs = async (req, res) => {
             }
         });
 
-        const jobs = response.data.jobs.map(job => ({
+        const jobs = response.data.jobs .map(job => ({
             name: job.name,
             url: job.url,
             color: job.color 
+            
         }));
 
         res.json({ jobs });
     } catch (error) {
         res.status(500).json({ error: "Erreur lors de la récupération des jobs", details: error.response?.data || error.message });
+        
     }
 };
